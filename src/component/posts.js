50d921dbad984434bd0d83cc0824fc6c5a4d4461
AmineZnaiguia <znaiguiamohamedamine@gmail.com>
2020-01-20 15:49:47 +0100

    first commit

---
+++ src/component/posts.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import axios from "axios";
+import Listpost from './listpost';
+
+export default class posts extends Component {
+ state = {
+       posts: [],  
+      }
+
+componentDidMount=()=>{this.getList()}
+      getList = () => {
+        axios
+          .get("https://jsonplaceholder.typicode.com/posts?userId=" + this.props.id)
+          .then(res => this.setState({ posts: res.data }))
+          .catch(err => console.log("err"));
+      };  
+    render() {
+        return (
+            <div>
+                  {this.state.posts.map(e =>
+             <Listpost post={e}/>)}
+            </div>
+        )
+    }
+}
